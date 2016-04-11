5edfba419cbd916168ed593bfe3209916b916373
Michał Bejger <michal.bejger@gmail.com>
2016-04-11 21:09:36 +0200

    Sensitivity scripts study: first version after VSR1

---
+++ sensitivity-scripts/prepare.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#--- General settings (please configure in mcsim.sh) -------------------
+band=${1}               # band number 
+h0=${2}                 # GW amplitude 
+#-----------------------------------------------------------------------
+code_home=${3}          # search codes 
+coin_home=${4}          # coincidence codes   
+script_home=${5}        # scripts 
+data=${6}               # input data
+list_of_frames=${7}     # file with frame list 
+lz4path=${8}            # archiver (coincidences) 
+ldlp=${9}               # yeppp library path 
+dt=${10}                # sampling time 
+howmany=${11}           # how many simulations
+gsize=${12}             # +- gsize around the grid
+#--- Coincidences ------------------------------------------------------
+reffr=${13}             # the reference frame (coincidences)
+cell=${14}              # Cell size (coincidences) 
+snrcut=${15}            # Signal-to-noise cutoff 
+mincoin=${16}           # Minimal no. of coincidences to register 
+#-----------------------------------------------------------------------
+
+cp ${script_home}/script_manyframes.sh script.sh
+#cp ${script_home}/clean.sh .
+#cp ${script_home}/get_summary.sh .
+
+# for the case when signal enters another band
+next_band=$((${band##+(0)}+1))
+next_band=$(printf "%03d\n" ${next_band})
+
+# replacements in script.sh 
+sed -i 's|BAND|'$band'|g;s|GSIZE|'$gsize'|g;s|REFFR|'$reffr'|g' script.sh
+sed -i 's|H0|'$h0'|g;s|CELL|'$cell'|g;s|DT|'$dt'|g' script.sh
+sed -i 's|SNRCUT|'$snrcut'|g;s|MINCOIN|'$mincoin'|g' script.sh 
+sed -i 's|DATA|'$data'|g;s|LOF|'$list_of_frames'|g' script.sh
+#sed -i 's|NEXTB|'$next_band'|g;s|LONF|'$list_of_frames'/'$next_band'.d|g' script.sh
+sed -i 's|LDLP|'$ldlp'|g;s|LZ4PATH|'$lz4path'|g' script.sh 
+
+# loop below prepares a number of catalogues: 01, 02,..., $howmany
+# makes symbolic links to programs
+# writes run.sh script for the queuing system 
+
+# removing the contents of an old run.sh
+if [ -e run.sh ]; then >run.sh; fi
+
+for dirs in $(seq -f "%02g" 1 $howmany); do
+
+	if [ ! -d "$dirs" ]; then 
+		mkdir $dirs
+	fi
+
+	echo $howmany $dirs 
+
+    # symbolic links to the codes 
+	ln -sf ${code_home}/gwsearch-cpu $dirs/search
+	ln -sf ${code_home}/sigen $dirs/sigen
+    ln -sf ${coin_home}/coincidences $dirs/coincidences 
+
+ 	ln -sf ../script.sh $dirs/script.sh
+	ln -sf ${script_home}/job.sub $dirs/job.sub
+
+	string=${h0}_${band}__${dirs}
+	echo "cd "${PWD}"/"${dirs}"; qsub -N "${string}" job.sub" >> run.sh
+
+done
+
+exit 0
---
+++ sensitivity-scripts/script_manyframes.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+#-------------------
+# Signal generation
+#-------------------
+
+sig_pars=''
+until [ -n "$sig_pars" ]; do  
+	sig_pars=$(./sigen -amp H0 -band BAND -dt DT -gsize GSIZE -reffr REFFR) 
+done 
+
+# Recovering the hemisphere number
+hemi=($sig_pars); hemi=${hemi[2]}
+
+# Saving injection details ''just in case''
+echo $sig_pars > sig_BAND
+
+num_of_frames=0 
+
+#rm BAND.snr 
+
+#----------------------------------
+# Adding signal to data and search  
+#----------------------------------
+
+for frame in $(cat LOF|sort -r); do 
+
+	LD_LIBRARY_PATH=LDLP ./search -data DATA -output . -ident ${frame} -band BAND -dt DT -addsig <(echo $sig_pars) --nocheckpoint 2>> BAND.snr 1>> BAND.out
+	let "num_of_frames += 1"
+	# ./search exits with exit(137) when the signal goes out of band 
+	if [[ $? -eq 137 ]]; then
+		last_frame=${frame}
+		break
+	fi	
+
+done 
+
+## this loop is used when the signal goes out of the initial band
+#if [[ $last_frame ]]; then 
+#
+#	for frame in $(awk '{if($1<='${last_frame}') print $1}' LONF|sort -r); do
+#
+#	        ./search -d DATA -i ${frame} -b NEXTB --whitenoise -a <(echo $sig_pars)	2>> BAND.snr
+#		let "num_of_frames += 1"
+#
+#	done 
+#fi 
+
+# signal-to-noise estimate (mean of the values generated during the search)
+# snr=$(grep "SNR:" BAND.snr | awk '{sum +=$2*$2} END {print sqrt(sum/'$num_of_frames')}')
+# sim_num=${PWD##*/} 
+
+#--------------
+# Coincidences
+#--------------
+
+mkdir BAND 
+
+# Calculate band frequency from band number
+fpo=$(echo "BAND DT"|awk '{print 10 + 0.96875*$1/(2*$2)}')
+
+# Searching for coincidences 
+for shi in {0..1}{0..1}{0..1}{0..1}; do
+	./coincidences -mincoin MINCOIN -snrcutoff SNRCUT -data . -trigname triggers_ -refloc DATA/REFFR -fpo $fpo -shift $shi -scale CELL -refr REFFR -dt DT -output BAND 2>> BAND/summary 1>> BAND.out  
+done
+
+# Selecting maximal coincidence for each pulsar among all 16 shifts (col. 5) 
+# if many, select the one with highest SNR (col. 10)  
+sort -rgk5 -gk10 BAND/summary | head -1 >> summary 
+
+# Cleanup (archiving and deleting the coi files) 
+for r in $(ls BAND/*.coi); do 
+	LZ4PATH/lz4 -6 ${r} ${r}.lz4 && gzip ${r}.lz4
+	if [ -f ${r}".lz4.gz" ]; then 
+    	rm $r
+    fi 
+done 
+
+## cleanup 
+#cd ../; rm -fr state* candidates/* *.e* *.o*
+#
+## write down the summary with the following info:
+## simulation_number 10_injection_parameters estimated_snr max_num_of_coincidences its_shift_direction total_number_of_frames number_of_cells estimated_injection_parameters
+## (description of the injection parameters in doc/readme_sigen.txt)
+#echo $sim_num $sig_pars $snr $max $dr $num_of_frames CELL $est >> H0_BAND_${sim_num}.sum
+
+exit 0
+
