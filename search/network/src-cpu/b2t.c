0f0708c9f4376a236d2f8e44251d9b7799feaf08
Paweł Ciecieląg <pci@camk.edu.pl>
2016-05-25 14:39:56 +0200

    float/double switch in b2t

--- search/network/src-cpu/b2t.c
+++ search/network/src-cpu/b2t.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define FLOAT double
 
 int main(int argc, char **argv) { 
-	
-	int i, j=0, data=5 ; 
-	FILE *datain ;  
-	float *cand ; 
-	
-    cand 		= (float *) calloc (data, sizeof(float));
-    	
-    // reading from the input datafile   
-	if ((datain = fopen (argv[1], "rb")) != NULL) {
 
-		while (!feof(datain)) {
-
-		   if((fread ((void *)(cand), sizeof (float), data, datain))==data)
-			
-				for(i=0; i<data; i++) 
-					printf("%.8le ", cand[i]) ;
-					
-					printf("\n") ;
-		 
-		}
-	
-	} else {
-		
-		perror (argv[1]);
-		return 1;
-	}
-
-    fclose(datain) ; 
+  
+  int i, j=0, data=5 ; 
+  FILE *datain ;  
+  FLOAT *cand ; 
+  
+  cand 		= (FLOAT *) calloc (data, sizeof(FLOAT));
+  
+  // reading from the input datafile   
+  if ((datain = fopen (argv[1], "rb")) != NULL) {
     
-	return 0 ; 
+    while (!feof(datain)) {
+      
+      if((fread ((void *)(cand), sizeof (FLOAT), data, datain))==data)
+	
+	for(i=0; i<data; i++) 
+	  printf("%.8le ", cand[i]) ;
+      
+      printf("\n") ;
+      
+    }
+    
+  } else {
+    
+    perror (argv[1]);
+    return 1;
+  }
+  
+  fclose(datain) ; 
+    
+  return 0 ; 
 }
