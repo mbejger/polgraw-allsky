53befa76a8cecc8e0d5de6e5d468d487ddac9542
Michal Bejger <bejger@camk.edu.pl>
2017-02-28 20:55:16 +0100

    Coincidences: read_coi.c outputs all coincidences with the highest multiplicity

--- coincidences/src/read_coi.c
+++ coincidences/src/read_coi.c
@@ -4,37 +4,45 @@
 int main(int argc, char **argv) { 
 	
 	int i, ops[256]; 
-    unsigned short int w, fra[256];  
+ 	unsigned short int w, whigh=0, fra[256];  
 	FILE *data;  
 	float mean[5]; 
     	
-  // reading from the input datafile   
+	// reading from the input datafile   
 	if ((data = fopen(argv[1], "rb")) != NULL) {
 
-    printf("# num_of_coincidences    mean_val_of_pars (f, s, d, a), snr    frame_num:trigger_num_in_trigger_file\n");
-    printf("# (see http://mbejger.github.io/polgraw-allsky/coincidences for details)\n");  
+	printf("# num_of_coincidences    mean_val_of_pars (f, s, d, a), snr    frame_num:trigger_num_in_trigger_file\n");
+	printf("# (see http://mbejger.github.io/polgraw-allsky/coincidences for details)\n");  
 
-    while(!feof(data)) {
+	while(!feof(data)) {
 
-      if(!fread(&w, sizeof(unsigned short int), 1, data)) { break; } 
-        fread(&mean, sizeof(float), 5, data);
-        fread(&fra, sizeof(unsigned short int), w, data); 
-        fread(&ops, sizeof(int), w, data);
+		// end if no more data 
+		if(!fread(&w, sizeof(unsigned short int), 1, data)) { break; }
 
-        printf("%hu ", w);
-        for(i=0; i<5; i++) printf("%le ", mean[i]);
-        for(i=0; i<w; i++) printf("%hu:%d ", fra[i], ops[i]);
-        printf("\n"); 
+		// first pass, set up the value of the highest coincidence 
+		if(!whigh) { whigh = w; }  
 
-    }  
+		// go out of the loop if coincidences lower than whigh appear  
+		if(w < whigh) { break; } 
+         
+		fread(&mean, sizeof(float), 5, data);
+		fread(&fra, sizeof(unsigned short int), w, data); 
+		fread(&ops, sizeof(int), w, data);
 
-  } else {
+		printf("%hu ", w);
+		for(i=0; i<5; i++) printf("%le ", mean[i]);
+		for(i=0; i<w; i++) printf("%hu:%d ", fra[i], ops[i]);
+		printf("\n"); 
+
+	}  
+
+	} else {
 		
 		perror (argv[1]);
 		return 1;
 	}
 
-  fclose(data); 
+	fclose(data); 
     
 	return 0; 
 
